843134e1325ca53b87f59055e4b3e88257e5c7af
franciole <franciole.bezerra@gmail.com>
2022-10-10 23:47:35 -0300

    Rota para componente HOME

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './component/nav/nav.component';
+import { HomeComponent } from './component/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: NavComponent
-  }
+    component: NavComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
