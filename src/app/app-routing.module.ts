30bb6404ebeafa873f0b5e3405580728641307a0
franciole <franciole.bezerra@gmail.com>
2022-10-11 23:54:36 -0300

    Rota para Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './component/nav/nav.component';
 import { HomeComponent } from './component/home/home.component';
 import { TecnicoListComponent } from './component/tecnico/tecnico-list/tecnico-list.component';
+import { LoginComponent } from './component/login/login.component';
 
 const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
   {
     path: '',
     component: NavComponent,
